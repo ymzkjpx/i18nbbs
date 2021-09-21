8b3184f5322b85f559027fb819eaee4c09514114
ymzkjpx <ymzkjpx@gmail.com>
2021-09-21 19:28:28 +0900

    テーブル設計の初期構想を記述

--- src/main/resources/db/migration/V1.01__chat_table.sql
+++ src/main/resources/db/migration/V1.01__chat_table.sql
@@ -1,12 +1,46 @@
-drop table if exists;
-drop SEQUENCE if EXISTS thread_id;
-drop SEQUENCE if EXISTS response_id;
-drop schema if exists chat CASCADE ;
-create schema chat;
+DROP TABLE if EXISTS original_language, original_message;
+DROP SEQUENCE if EXISTS thread_id;
+DROP SEQUENCE if EXISTS response_id;
+DROP SCHEMA if EXISTS chat CASCADE;
+CREATE SCHEMA chat;
 
-CREATE table if not EXISTS chat.original
+CREATE TABLE if NOT EXISTS chat.original_language
 (
-    original_id VARCHAR (24) PRIMARY KEY
+    lang VARCHAR (30) PRIMARY KEY
+);
 
-)
-CREATE TABLE IF NOT EXISTS `product_menbers` (
+CREATE SEQUENCE thread_id;
+CREATE SEQUENCE response_id;
+create SEQUENCE threti_id;
+
+CREATE TABLE if NOT EXISTS chat.original_message
+(
+    id VARCHAR (24) PRIMARY KEY
+    , message VARCHAR(2048) NOT NULL
+    , lang VARCHAR(30) NOT NULL REFERENCES chat.original_language
+    , created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE if NOT EXISTS chat.thread
+(
+    thread_id INTEGER PRIMARY KEY
+    , created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE table if NOT EXISTS chat.threti
+(
+    threti_id INTEGER PRIMARY KEY
+    , thread_id INTEGER REFERENCES chat.thread
+    , title varchar(100) NOT NULL
+    , lang VARCHAR (30) NOT NULL REFERENCES chat.original_language
+    , created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE if NOT EXISTS chat.response
+(
+    response_id INTEGER PRIMARY KEY
+    , origina_message_id VARCHAR (24) REFERENCES chat.original_message
+    , thread_id INTEGER REFERENCES chat.thread
+    , threti_id INTEGER REFERENCES chat.threti
+    , created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
