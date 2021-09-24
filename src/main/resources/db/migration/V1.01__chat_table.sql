fd8e260acfe341f2a80cb782bee650d66bd6b5f5
ymzkjpx <ymzkjpx@gmail.com>
2021-09-24 19:07:37 +0900

    テーブル設計を一方向性なタイプに変更

--- src/main/resources/db/migration/V1.01__chat_table.sql
+++ src/main/resources/db/migration/V1.01__chat_table.sql
@@ -1,40 +1,41 @@
-DROP TABLE if EXISTS original_language, original_message, thread, thread_title, response;
-DROP SEQUENCE if EXISTS chat.thread_id;
-DROP SEQUENCE if EXISTS chat.response_id;
-DROP SEQUENCE if EXISTS chat.thread_title_id;
-DROP SCHEMA if EXISTS chat CASCADE;
+DROP TABLE IF EXISTS thread, thread_theme, response, original_message;
+DROP SCHEMA IF EXISTS chat CASCADE;
 CREATE SCHEMA chat;
 
+DROP SEQUENCE IF EXISTS chat.thread_id;
 CREATE SEQUENCE chat.thread_id;
+CREATE TABLE IF NOT EXISTS chat.thread
+(
+    thread_id INTEGER PRIMARY KEY ,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP SEQUENCE IF EXISTS chat.thread_theme_id;
+CREATE SEQUENCE chat.thread_theme_id;
+CREATE TABLE IF NOT EXISTS chat.thread_theme
+(
+    thread_theme_id INTEGER PRIMARY KEY ,
+    thread_id INTEGER REFERENCES chat.thread (thread_id) ,
+    title VARCHAR(100) NOT NULL ,
+    title_owner VARCHAR(60) NOT NULL ,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP SEQUENCE IF EXISTS chat.response_id;
 CREATE SEQUENCE chat.response_id;
-create SEQUENCE chat.thread_title_id;
-
-CREATE TABLE if NOT EXISTS chat.original_message
+CREATE TABLE IF NOT EXISTS chat.response
 (
-    id VARCHAR (24) PRIMARY KEY
-    , message VARCHAR(2048) NOT NULL
-    , created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    response_id INTEGER PRIMARY KEY ,
+    thread_id INTEGER REFERENCES chat.thread (thread_id) ,
+    response_order INTEGER NOT NULL ,
+    response_owner varchar(60) NOT NULL ,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE if NOT EXISTS chat.thread
+CREATE TABLE IF NOT EXISTS chat.original_message
 (
-    thread_id INTEGER PRIMARY KEY
-    , created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE table if NOT EXISTS chat.thread_title
-(
-    thread_title_id INTEGER PRIMARY KEY
-    , thread_id INTEGER REFERENCES chat.thread (thread_id)
-    , title varchar(100) NOT NULL
-    , created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE if NOT EXISTS chat.response
-(
-    response_id INTEGER PRIMARY KEY
-    , original_message_id VARCHAR (24) REFERENCES chat.original_message
-    , thread_id INTEGER REFERENCES chat.thread (thread_id)
-    , thread_title_id INTEGER REFERENCES chat.thread_title (thread_title_id)
-    , created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    original_message_id VARCHAR(24) PRIMARY KEY ,
+    response_id INTEGER REFERENCES chat.response (response_id) ,
+    original_message VARCHAR(1000) NOT NULL ,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
--- src/main/resources/db/migration/V1.03__chat_data.sql
+++ src/main/resources/db/migration/V1.03__chat_data.sql
@@ -1,32 +1,32 @@
-INSERT INTO chat.original_message (id, message)
-VALUES
-('2af6-f9e1-4a31-8c2e-a7c0', 'ダミースレッド1のスレッドタイトル本文'),
-('2ef7-9f1c-4762-824a-0aaa', 'dummy text2'),
-('e54e-4b10-485e-8389-5459', '虚拟文本3'),
-('3f40-b10b-42fa-97b8-e0d6', '仮文章4'),
-('f868-7121-4de5-aa84-e537', 'dummy text5'),
-('bf34-c111-4d60-a9cb-e657', '仮文章6'),
-('82a4-19a1-46cb-b697-2c3a', '虚拟文本7'),
-('789f-f427-47f8-8f3f-26d9', 'dummy text8'),
-('d98b-2395-4764-8490-2d85', '仮文章9'),
-('3e8c-0be8-4743-aca3-f322', '仮文章10')
-;
-
 INSERT INTO chat.thread (thread_id)
 VALUES
 ((SELECT nextval('chat.thread_id'))) ;
 
-INSERT INTO chat.thread_title (thread_title_id, thread_id, title)
+INSERT INTO chat.thread_theme (thread_theme_id, thread_id, title, title_owner)
 VALUES
-    ((SELECT NEXTVAL('chat.thread_title_id')), (SELECT CURRVAL('chat.thread_id')), 'ダミースレッドタイトル1')
+    ((SELECT NEXTVAL('chat.thread_theme_id')), (SELECT CURRVAL('chat.thread_id')), 'スレッドのお題', 'スレッドの設立者名')
 ;
 
-INSERT INTO chat.response (response_id, original_message_id, thread_id, thread_title_id)
+INSERT INTO chat.response (response_id, thread_id, response_order, response_owner)
 VALUES
-((SELECT NEXTVAL('chat.response_id')), '2af6-f9e1-4a31-8c2e-a7c0', (SELECT CURRVAL('chat.thread_id')), (SELECT CURRVAL('chat.thread_title_id')))
-  ,((SELECT NEXTVAL('chat.response_id')), '2ef7-9f1c-4762-824a-0aaa', (SELECT CURRVAL('chat.thread_id')), (SELECT CURRVAL('chat.thread_title_id')))
-  ,((SELECT NEXTVAL('chat.response_id')), 'e54e-4b10-485e-8389-5459', (SELECT CURRVAL('chat.thread_id')), (SELECT CURRVAL('chat.thread_title_id')))
-  ,((SELECT NEXTVAL('chat.response_id')), '3f40-b10b-42fa-97b8-e0d6', (SELECT CURRVAL('chat.thread_id')), (SELECT CURRVAL('chat.thread_title_id')))
-  ,((SELECT NEXTVAL('chat.response_id')), 'f868-7121-4de5-aa84-e537', (SELECT CURRVAL('chat.thread_id')), (SELECT CURRVAL('chat.thread_title_id')))
-  ,((SELECT NEXTVAL('chat.response_id')), 'bf34-c111-4d60-a9cb-e657', (SELECT CURRVAL('chat.thread_id')), (SELECT CURRVAL('chat.thread_title_id')))
+((SELECT NEXTVAL('chat.response_id')), (SELECT CURRVAL('chat.thread_id')), 1, 'Bob'),
+((SELECT NEXTVAL('chat.response_id')), (SELECT CURRVAL('chat.thread_id')), 2, 'Carol'),
+((SELECT NEXTVAL('chat.response_id')), (SELECT CURRVAL('chat.thread_id')), 3, 'Dave'),
+((SELECT NEXTVAL('chat.response_id')), (SELECT CURRVAL('chat.thread_id')), 4, 'Eve'),
+((SELECT NEXTVAL('chat.response_id')), (SELECT CURRVAL('chat.thread_id')), 5, 'Bob'),
+((SELECT NEXTVAL('chat.response_id')), (SELECT CURRVAL('chat.thread_id')), 6, 'Carol')
+;
+
+INSERT INTO chat.original_message (original_message_id, response_id, original_message)
+VALUES
+    ('2af6-f9e1-4a31-8c2e-a7c0', 1, '本文1'),
+    ('2ef7-9f1c-4762-824a-0aaa', 1, 'dummy text2'),
+    ('e54e-4b10-485e-8389-5459', 1, '虚拟文本3'),
+    ('3f40-b10b-42fa-97b8-e0d6', 1, '仮文章4'),
+    ('f868-7121-4de5-aa84-e537', 1, 'dummy text5'),
+    ('bf34-c111-4d60-a9cb-e657', 1, '仮文章6'),
+    ('82a4-19a1-46cb-b697-2c3a', 1, '虚拟文本7'),
+    ('789f-f427-47f8-8f3f-26d9', 1, 'dummy text8'),
+    ('d98b-2395-4764-8490-2d85', 1, '仮文章9'),
+    ('3e8c-0be8-4743-aca3-f322', 1, '仮文章10')
 ;
